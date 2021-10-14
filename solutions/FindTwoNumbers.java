c3cd8028045bc3b4a1b85daa38b299d9378813a0
bobwolf999 <80534546+bobwolf999@users.noreply.github.com>
2021-10-14 19:56:42 +0200

    initial save01

--- solutions/FindTwoNumbers.java
+++ solutions/FindTwoNumbers.java
@@ -1,23 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindTwoNumbers {
 
-    public static int[] findTwoElements(int arr[], int n){
+    public static int findTwoElements(int arr[], int n){
 
         int[] a = {0, 0};
         for(int i = 0 ; i < n - 1 ; i++)
             for(int j = i + 1 ; j < n ; j++){
                 if( arr[i] == arr[j]){
                     a[1] = arr[i];
+                    System.out.println(a[1]);
                 }
             }
-        return a;
+        return a[1];
     }
 
     // Driver code
     public static void main(String[] args)
     {
-        int arr[] = { 1, 9, 3, 10, 4, 20, 2 };
+        int arr[] = { 1, 9, 9, 10, 4, 2, 2 };
         int n = arr.length;
 
-        System.out.println("  "+ findTwoElements(arr, n).toString());
+        System.out.println("  "+ findTwoElements(arr, n));
     }
 }
