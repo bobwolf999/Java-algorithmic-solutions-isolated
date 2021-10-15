05041f1683f0d3f6cef42114008c24d43fe268f1
bobwolf999 <80534546+bobwolf999@users.noreply.github.com>
2021-10-15 17:12:26 +0200

    initial save02

--- solutions/FindTwoNumbers.java
+++ solutions/FindTwoNumbers.java
@@ -16,12 +16,26 @@ public class FindTwoNumbers {
         return a[1];
     }
 
-    // Driver code
+    public static void twoSum(int arr[], int target){
+        for(int i = 0 ; i < arr.length - 1 ; i++)
+            for(int j = i + 1 ; j < arr.length ; j++){
+                if(arr[i] + arr[j] == target){
+                    System.out.println(arr[i] + "," + arr[j]);
+                    break;
+                }
+            }
+    }
+
+
     public static void main(String[] args)
     {
         int arr[] = { 1, 9, 9, 10, 4, 2, 2 };
         int n = arr.length;
+        int target = 6;
 
-        System.out.println("  "+ findTwoElements(arr, n));
+
+        findTwoElements(arr, n);
+        twoSum(arr, target);
     }
 }
+
